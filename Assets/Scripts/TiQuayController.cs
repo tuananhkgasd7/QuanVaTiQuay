1cc52001c5f889486841c7056b5a677fd428fdef
tuananhkgasd7 <tuananhkgasd7@gmail.com>
2021-12-24 07:36:14 +0700

    hoàn thiện hình ảnh map 1

--- Assets/Scripts/TiQuayController.cs
+++ Assets/Scripts/TiQuayController.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class TiQuayController : MonoBehaviour
 {
+    //Speed
+    public float speed = 4.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+  
+    //Animator
+    Animator anim;
+    Vector2 direction = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 moving = new Vector2(horizontal, vertical);
+        if(!Mathf.Approximately(moving.x, 0.0f) || !Mathf.Approximately(moving.y, 0.0f)){
+            direction = moving.normalized;
+        }
+        anim.SetFloat("Move X", direction.x);
+        anim.SetFloat("Move Y", direction.y);
+        anim.SetFloat("Speed", moving.magnitude);
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        //     Launch();
+        // }
+    }
+
+    private void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position.x += (0.05f * horizontal);
-        position.y += (0.05f * vertical);
-        transform.position = position;
+        position.x += (speed * horizontal) * Time.deltaTime;
+        position.y += (speed * vertical) * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
