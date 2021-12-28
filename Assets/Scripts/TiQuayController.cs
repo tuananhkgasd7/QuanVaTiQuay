244b20c36923c83fc9b12c0a587d364190058736
Nguyễn Lê Cường <huenguyenht1962@gmail.com>
2021-12-28 11:15:56 +0700

    Update asset

--- Assets/Scripts/TiQuayController.cs
+++ Assets/Scripts/TiQuayController.cs
@@ -12,6 +12,7 @@ public class TiQuayController : MonoBehaviour
   
     //Animator
     Animator anim;
+    Vector2 direction = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,14 +27,13 @@ public class TiQuayController : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
 
         Vector2 moving = new Vector2(horizontal, vertical);
-        Vector2 direction = Vector2.zero;
         if(!Mathf.Approximately(moving.x, 0.0f) || !Mathf.Approximately(moving.y, 0.0f)){
             direction = moving.normalized;
         }
         anim.SetFloat("Move X", direction.x);
         anim.SetFloat("Move Y", direction.y);
         anim.SetFloat("Speed", moving.magnitude);
-
+        
         // if(Input.GetKeyDown(KeyCode.Space)){
         //     Launch();
         // }
